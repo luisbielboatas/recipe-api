b47975e0b0b8cdbde9f6117308d51cdc9f1ed78c
luisbiel <luis.biel@travelperk.com>
2020-06-29 19:26:32 +0200

    Added recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -55,3 +55,12 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipi_str(self):
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Caracoles a la llauna',
+            time_minutes=5,
+            price=5.00
+        )
+        self.assertEqual(str(recipe), recipe.title)
